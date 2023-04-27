8cdc769b437ddf7b3a3b78ba064ebd1df2a1a53e
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-04-27 13:54:49 -0700

    using  instead of import so underlying packages will work

--- src/ThermalSystem_Models.jl
+++ src/ThermalSystem_Models.jl
@@ -1,8 +1,9 @@
 module ThermalSystem_Models
-import Roots, DataFrames, XSteam, Printf
-import Graphs, GraphPlot , GraphRecipes
-import Plots, GraphRecipes, LinearAlgebra
-import  PlotlyJS
+
+using Roots, DataFrames, XSteam, Printf
+using Graphs, Plots
+using GraphPlot 
+using GraphRecipes, LinearAlgebra
 
 include("01-fluid_data.jl")
 include("02-nodes.jl")
